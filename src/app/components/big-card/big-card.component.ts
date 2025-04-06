94f678df9801fae6c5f49a0a3906e2fa63bc1140
gsmereka <gsmereka200@gmail.com>
2025-04-05 21:59:12 -0300

    feat: update big-card, add big-card css

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -15,13 +15,15 @@ export class BigCardComponent {
   @Input()
   Id:string="0"
 
-  placeHolder:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/800px-Placeholder_view_vector.svg.png"
-  
+  imgPlaceHolder:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/800px-Placeholder_view_vector.svg.png"
+  titlePlaceHolder:string = "Breaking News: News Title"
   constructor() { }
 
   ngOnInit(): void {
     if (this.photoCover == "")
-      this.photoCover = this.placeHolder
+      this.photoCover = this.imgPlaceHolder
+    if (this.cardTitle == "")
+      this.cardTitle = this.titlePlaceHolder
   }
 
 }
