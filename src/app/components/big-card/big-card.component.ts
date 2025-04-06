0d470ee8da0df0ca9f99a47cfeefc4b60db52b46
gsmereka <gsmereka200@gmail.com>
2025-04-05 21:49:36 -0300

    feat: add photo and title on big-card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string= ""
+  @Input()
+  Id:string="0"
+
+  placeHolder:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/800px-Placeholder_view_vector.svg.png"
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    if (this.photoCover == "")
+      this.photoCover = this.placeHolder
+  }
 
 }
